b8381c095cc7680cdcca4af16d04f29f30db8920
Vince <vilev39@ulaval.ca>
2024-02-13 16:48:32 -0500

    1st push

---
+++ src/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NavBar() {
+    return <>
+        <div>CV</div>
+        <div>À propos</div>
+        <div>Projets</div>
+        <div>LinkedIn</div>
+    </>
+}
